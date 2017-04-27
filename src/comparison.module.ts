42a3c715ceb46dd71450c7dd83e08822519de35a
Fabian Vilers <fabian.vilers@dev-one.com>
2017-04-27 15:55:57 +0200

    Add missing LowerThanOrEqualDirective to declarations

--- src/comparison.module.ts
+++ src/comparison.module.ts
@@ -14,6 +14,7 @@ import {
     EqualToDirective,
     GreaterThanDirective,
     GreaterThanOrEqualToDirective,
+    LowerThanOrEqualDirective,
     LowerThanDirective,
     NotEqualToDirective
   ],
