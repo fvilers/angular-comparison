d09a9ad11be12ef37c89cd968d6c21c0dfaabd08
Fabian Vilers <fabian.vilers@dev-one.com>
2017-11-06 17:10:12 +0100

    Create the angular app

--- src/directives/index.ts
+++
@@ -1,7 +0,0 @@
-export { ComparisonDirective } from './comparison.directive';
-export { EqualToDirective } from './equal-to.directive';
-export { GreaterThanOrEqualToDirective } from './greater-than-or-equal-to.directive';
-export { GreaterThanDirective } from './greater-than.directive';
-export { LowerThanOrEqualDirective } from './lower-than-or-equal.directive';
-export { LowerThanDirective } from './lower-than.directive';
-export { NotEqualToDirective } from './not-equal-to.directive';
